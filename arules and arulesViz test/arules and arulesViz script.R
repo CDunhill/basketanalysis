f2089ffcf2298e01d62d69e7cd38275308eda6ac
Chris Dunhill <chrisdunhill@gmail.com>
2017-11-07 17:04:36 +0000

    Making progress

--- arules and arulesViz test/arules and arulesViz script.R
+++ arules and arulesViz test/arules and arulesViz script.R
@@ -2,16 +2,19 @@
 # Trying this tutorial
 # http://www.salemmarafi.com/code/market-basket-analysis-with-r/
 
+# NB. Can we bring the SQL query into the R code? Would be great to make it dynamic with selectable params
+# Here's the query I used:
+# "Documents\SQL Server Management Studio\Basket or Transaction Analysis\TX Query for ARULES analysis (simpler).sql"
+
 # Load the libraries
 library(arules)
 library(arulesViz)
-library(datasets)
+library(datasets) # is this necessary?
 
 # Load the data set
 library(readr)
-dataset <- read.csv('LW_TX_data.csv', header = FALSE, fileEncoding="UTF-8-BOM") # fileEncoding seemed to get rid of some odd characters
-View(dataset)
+mwlw = read.transactions("LW_TX_data.csv", sep = ",")
+View(mwlw)
 
+itemFrequencyPlot(mwlw,topN=20,type="absolute")
 
-
-data(LW_TX_data)
