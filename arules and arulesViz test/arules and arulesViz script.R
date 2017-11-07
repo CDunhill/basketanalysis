cf57bd4bd762a56746e73bfaa318936d7049a3e9
Chris Dunhill <chrisdunhill@gmail.com>
2017-11-07 16:26:50 +0000

    CSV commit

--- arules and arulesViz test/arules and arulesViz script.R
+++ arules and arulesViz test/arules and arulesViz script.R
@@ -1,8 +1,17 @@
 
+# Trying this tutorial
+# http://www.salemmarafi.com/code/market-basket-analysis-with-r/
+
 # Load the libraries
 library(arules)
 library(arulesViz)
 library(datasets)
 
 # Load the data set
-data(Groceries)
+library(readr)
+dataset <- read.csv('LW_TX_data.csv', header = FALSE, fileEncoding="UTF-8-BOM") # fileEncoding seemed to get rid of some odd characters
+View(dataset)
+
+
+
+data(LW_TX_data)
