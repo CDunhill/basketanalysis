41a2040c0a294831a365d881bbb676f5efb98398
Chris Dunhill <chrisdunhill@gmail.com>
2017-11-10 11:08:18 +0000

    Network graph with arulesViz added. Now just need to interpret.

--- arules and arulesViz test/arules and arulesViz script.R
+++ arules and arulesViz test/arules and arulesViz script.R
@@ -25,11 +25,11 @@ itemFrequencyPlot(mwlw,topN=40,type="absolute")
 # The SUPPORT is how many times the product combination appears in our transaction list.
 # So 0.01 would only show where the particular combination ('LHS', or antecedent) appears in >=1% transactions.
 # The CONFIDENCE is simply how often the rule is shown to be true
-rules <- apriori(mwlw, parameter = list(supp = 0.001, conf = 0.6,maxlen=3))  # max len makes for more concise rules
+rules <- apriori(mwlw, parameter = list(supp = 0.0015, conf = 0.5,maxlen=5))  # max len makes for more concise rules
 rules<-sort(rules, by="confidence", decreasing=TRUE)
 
 # Show the top 5 rules, but only 2 digits
-options(digits=2)
+options(digits=2) # Part of base package; digits controls no. sig digits when printing numeric values
 inspect(rules[1:10])
 
 # Redundancies
@@ -40,11 +40,33 @@ inspect(rules[1:10])
 
 # (I DON'T THINK THIS WORKS IN ITS CURRENT FORM)
 
+# DO NOT RUN...
 subset.matrix <- is.subset(rules, rules)
 subset.matrix[lower.tri(subset.matrix, diag=T)] <- NA
 redundant <- colSums(subset.matrix, na.rm=T) >= 1
 rules.pruned <- rules[!redundant]
 rules<-rules.pruned
+
+# TARGETING ITEMS
+# EG. What are customers likely to buy before buying KID-ACC-GLO
+#     What are customers likely to buy if they purchase KID-ACC-GLO?
+rules<-apriori(data=mwlw, parameter=list(supp=0.0015,conf = 0.4), 
+               appearance = list(default="lhs",rhs="KID-ACC-GLO"),
+               control = list(verbose=F))
+rules<-sort(rules, decreasing=TRUE,by="confidence")
+inspect(rules[1:10])
+
+# Likewise, we can set the left hand side to be “KID-ACC-GLO” and find its antecedents.
+rules<-apriori(data=mwlw, parameter=list(supp=0.0015,conf = 0.05,minlen=2), 
+               appearance = list(default="rhs",lhs="KID-ACC-GLO"),
+               control = list(verbose=F))
+rules<-sort(rules, decreasing=TRUE,by="confidence")
+inspect(rules[1:5])
+
+
+# VISUALISATION
+library(arulesViz)
+plot(rules, method = "graph", engine = "interactive")
 
 
 
