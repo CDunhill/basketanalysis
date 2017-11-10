f36c82d03d80d087472f60a73390f2797bf3adef
Chris Dunhill <chrisdunhill@gmail.com>
2017-11-10 16:25:10 +0000

    Still playing with visNetwork. Can't quite work out how to colour the nodes correctly - I think I'm approaching it the wrong way.

--- arules and arulesViz test/arules and arulesViz script.R
+++ arules and arulesViz test/arules and arulesViz script.R
@@ -2,6 +2,8 @@
 # Trying this tutorial
 # http://www.salemmarafi.com/code/market-basket-analysis-with-r/
 
+# Here's a great guide to visNetwork library http://datastorm-open.github.io/visNetwork/nodes.html
+
 # NB. Can we bring the SQL query into the R code? Would be great to make it dynamic with selectable params
 # Here's the query I used:
 # "Documents\SQL Server Management Studio\Basket or Transaction Analysis\TX Query for ARULES analysis (simpler).sql"
@@ -27,7 +29,7 @@ itemFrequencyPlot(mwlw,topN=40,type="absolute")
 # The SUPPORT is how many times the product combination appears in our transaction list.
 # So 0.01 would only show where the particular combination ('LHS', or antecedent) appears in >=1% transactions.
 # The CONFIDENCE is simply how often the rule is shown to be true
-rules <- apriori(mwlw, parameter = list(supp = 0.0005, conf = 0.3,maxlen=5))  # max len makes for more concise rules
+rules <- apriori(mwlw, parameter = list(supp = 0.002, conf = 0.7,maxlen=3))  # max len makes for more concise rules
 rules<-sort(rules, by="confidence", decreasing=TRUE)
 
 ig <- plot( rules, method="graph", control=list(type="items"))
@@ -40,14 +42,15 @@ saveAsGraph( rules, file = tf, format = "dot" )
 
 # let's bypass saveAsGraph and just use our igraph
 ig_df <- get.data.frame( ig, what = "both" )
+
 visNetwork(
-  nodes = data.frame(
-    id = ig_df$vertices$name
-    ,value = ig_df$vertices$support # could change to lift or confidence
-    ,title = ifelse(ig_df$vertices$label == "",ig_df$vertices$name, ig_df$vertices$label)
-    ,ig_df$vertices
-  )
-  , edges = ig_df$edges
+  nodes = data.frame(id = ig_df$vertices$name,
+                     color = c("darkred", "grey", "orange"),
+                     shadow = c(FALSE, TRUE, FALSE),
+                     value = ig_df$vertices$support,
+                     title = ifelse(ig_df$vertices$label == "",ig_df$vertices$name, ig_df$vertices$label),
+                     ig_df$vertices)
+  ,edges = ig_df$edges
 ) %>%
   visOptions( highlightNearest = T )
 
